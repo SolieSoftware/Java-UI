02dc0e69e35e05552ec5ee983e1e6081f32857ff
solsh <sol.shortland@gmail.com>
2023-09-30 18:39:05 +0100

    All relevant functionality added to movie quiz window. Next points of order are to set up suites of unit tests as part of the program build.

--- src/com/codebind/MovieQuiz.java
+++ src/com/codebind/MovieQuiz.java
@@ -3,44 +3,76 @@ package com.codebind;
 import javax.swing.*;
 import java.awt.*;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Collections;
 
 public class MovieQuiz {
     JFrame movieFrame = new JFrame("Movie Quiz!!");
 
-    JPanel[] moviePanel = new JPanel[10];
+    JPanel[] moviePanel = new JPanel[5];
+
+    JPanel[] answerPanel = new JPanel[5];
+
+
 
     private String url = "jdbc:postgresql://localhost:5432/movies";
     private String username = "postgres";
-    private String password = "Joke100v";
+    private String password = "Joker100v";
 
-    ArrayList<String> movie_titles = new ArrayList<>();
+    String[][] movie = new String[100][3];
+
+    String[] questionFormats = {
+            "What year was X made in?",
+            "Who directed X?"
+    };
+
+    JLabel[] questions = new JLabel[5];
+    ArrayList<String> questionAnswer;
+
+    JRadioButton[][] answerOptions = new JRadioButton[5][4];
+    ButtonGroup[] answerGroups =  new ButtonGroup[5];
+
+    JButton submitButton = new JButton("Submit Answers");
+
+    ArrayList<String> correctAnswers = new ArrayList<>();
 
 
 
 
     MovieQuiz() {
 
-        movieFrame.setSize(new Dimension(500,500));
+        movieFrame.setPreferredSize(new Dimension(1000,700));
         movieFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        movieFrame.setLayout(new BorderLayout());
+        movieFrame.setLocation(500,500);
+        movieFrame.setLayout(new FlowLayout(FlowLayout.CENTER));
 
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
             Statement statement = connection.createStatement();
             String sqlQuery = "Select * FROM public.imdb_top_100";
             ResultSet resultSet = statement.executeQuery(sqlQuery);
-            String temp = "";
-
+            String title;
+            String year;
+            String director;
+            int count = 0;
             while (resultSet.next()) {
-                temp = resultSet.getString("original_movie_title");
-                movie_titles.add(temp);
-            }
+                title = resultSet.getString("original_title");
+                year = String.valueOf(resultSet.getInt("year"));
+                director = resultSet.getString("director");
 
+                movie[count][0] = title;
+                movie[count][1] = year;
+                movie[count][2] = director;
+
+                count+=1;
+            }
             resultSet.close();
             statement.close();
             connection.close();
@@ -57,25 +89,64 @@ public class MovieQuiz {
 
         for (int i = 0; i<5; i++) {
             moviePanel[i] = new JPanel();
-            moviePanel[i].setPreferredSize(new Dimension(100,100));
+            moviePanel[i].setPreferredSize(new Dimension(800,100));
+            moviePanel[i].setLayout(new BorderLayout());
+
+            answerPanel[i] = new JPanel();
+            answerPanel[i].setPreferredSize(new Dimension(800, 50));
+            answerPanel[i].setLayout(new FlowLayout());
+
+            questionAnswer = getQuestion();
+
+            questions[i] = new JLabel(questionAnswer.get(4));
+            questions[i].setLocation(400, 20);
+
+            answerGroups[i] = new ButtonGroup();
+
+
+
+            for (int j=0; j<4; j++) {
+                answerOptions[i][j] = new JRadioButton();
+                answerOptions[i][j].setActionCommand(questionAnswer.get(j));
+                answerOptions[i][j].setText(questionAnswer.get(j));
+                answerGroups[i].add(answerOptions[i][j]);
+                answerPanel[i].add(answerOptions[i][j]);
+            }
+
+
+
+            moviePanel[i].add(questions[i], BorderLayout.NORTH);
+            moviePanel[i].add(answerPanel[i], BorderLayout.SOUTH);
+
+            movieFrame.add(moviePanel[i]);
 
         }
+        submitButton.setPreferredSize(new Dimension(200,50));
 
-        moviePanel[0].setBackground(Color.red);
-        moviePanel[1].setBackground(Color.green);
-        moviePanel[2].setBackground(Color.blue);
-        moviePanel[3].setBackground(Color.magenta);
-        moviePanel[4].setBackground(Color.yellow);
+        submitButton.addActionListener(new ActionListener() {
+
+            @Override
+
+            public void actionPerformed(ActionEvent e) {
+                int numCorrect = 0;
+                String text;
+                for (int i=0; i<5; i++) {
+
+                    ButtonModel selectedButton = answerGroups[i].getSelection();
+                    text = selectedButton.getActionCommand();
+                    if (text.equals(correctAnswers.get(i))) {
+                        numCorrect += 1;
+                    }
+
+                }
+
+                JOptionPane.showMessageDialog(null, "You got " + numCorrect + "/5");
+
+            }
+        });
 
 
-        movieFrame.add(moviePanel[0], BorderLayout.NORTH);
-        movieFrame.add(moviePanel[1], BorderLayout.SOUTH);
-        movieFrame.add(moviePanel[2], BorderLayout.CENTER);
-        movieFrame.add(moviePanel[3], BorderLayout.WEST);
-        movieFrame.add(moviePanel[4], BorderLayout.EAST);
-
-
-
+        movieFrame.add(submitButton);
 
 
 
@@ -85,4 +156,39 @@ public class MovieQuiz {
 
 
     }
+
+    public ArrayList<String> getQuestion() {
+
+        Random rand = new Random();
+        int num = rand.nextInt(2);
+        String question = questionFormats[num];
+        int num2 = rand.nextInt(100);
+        String movieTitle = movie[num2][0];
+
+
+        ArrayList<String> answers = new ArrayList<>();
+        answers.add(movie[num2][num+1]);
+        correctAnswers.add(movie[num2][num+1]);
+
+
+
+        question = question.replace("X", movieTitle);
+        while (answers.size() != 4) {
+            num2 = rand.nextInt(100);
+            if (movie[num2][num+1] != answers.get(0)) {
+                answers.add(movie[num2][num+1]);
+            }
+        }
+
+        Collections.shuffle(answers);
+
+        answers.add(question);
+
+        return answers;
+
+    }
+
+
+
+
 }
